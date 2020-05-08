1358fbf10798b1e3474fa406dbde8b24a493db31
Jean Ruggiero <jeanruggiero@gmail.com>
2020-05-07 20:58:37 -0700

    Updated allowed hosts

--- og_backend/settings.py
+++ og_backend/settings.py
@@ -4,7 +4,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.eba-bqurfhwn.us-west-2.elasticbeanstalk.com']
 
 CORS_ORIGIN_WHITELIST = []
 CSRF_TRUSTED_ORIGINS = []
