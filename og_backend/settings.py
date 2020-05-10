015245795467bca9660f9960ec24662e7a9c5502
Jean Ruggiero <jeanruggiero@gmail.com>
2020-05-09 17:55:12 -0700

    Added https redirect to settings

--- og_backend/settings.py
+++ og_backend/settings.py
@@ -8,3 +8,7 @@ ALLOWED_HOSTS = ['django-env.eba-bqurfhwn.us-west-2.elasticbeanstalk.com']
 
 CORS_ORIGIN_WHITELIST = ['https://master.d1tifnnvu21b6i.amplifyapp.com/']
 CSRF_TRUSTED_ORIGINS = ['https://master.d1tifnnvu21b6i.amplifyapp.com/']
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
